6531ee98a7151e67be1399a7b651379d379ee86d
LynnDelaere <lynn.delaere@hotmail.be>
2024-09-16 18:24:50 +0200

    Add java for age

---
+++ recources/js/javascript.js
@@ -0,0 +1,14 @@
+const birthDate = '1994-07-20';
+
+function calculateAge(birthDate) {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDifference = today.getMonth() - birth.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById('age').textContent = calculateAge(birthDate);
